0b234164a59f695fdeb0f3bedc4b1f6727309e67
The Wizard of Hahz <info@luvnft.com>
2024-05-31 11:54:07 -0400

    updated

--- utils/contract.ts
+++ utils/contract.ts
@@ -3,7 +3,7 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { contractABI } from "./contractABI";
 
-const contractAddress = "<contract_addrss>";
+const contractAddress = "0x4B21A49a40BEf71477B7B89eDaFe8049016B2E65";
 
 export const contract = getContract({
     client: client,
